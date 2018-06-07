e7c780308fa98723045ed1127616273d834640d0
Juan Montoya <jcmontoya@gmail.com>
2018-06-07 18:34:55 +0000

    Added link to navigation directive for 'All Stocks'.

--- public/angular-app/stock-data-factory/stock-data-factory.js
+++ public/angular-app/stock-data-factory/stock-data-factory.js
@@ -1,3 +1,19 @@
 angular.module('cdfinance').factory('stockDataFactory', stockDataFactory);
 
-function
+function stockDataFactory($http) {
+    return {
+      stockList : stockList
+    };
+    
+    function stockList() {
+        return $http.get('/api/stocks').then(complete).catch(failed);
+    }
+    
+    function complete(response) {
+        return response.data;
+    }
+    
+    function failed(error) {
+        console.log(error.statusText);
+    }
+}
--- public/angular-app/stock-list/stock-list-controller.js
+++ public/angular-app/stock-list/stock-list-controller.js
@@ -1,3 +1,10 @@
-angular.module('cdfinance').controller('StocksController', StocksContorller);
+angular.module('cdfinance').controller('StocksController', StocksController);
 
-function StocksController()
+function StocksController(stockDataFactory) {
+    var vm = this;
+    vm.title = "All Stocks";
+    stockDataFactory.stockList().then(function(response) {
+        console.log(response);
+        vm.stocks = response;
+    });
+}
