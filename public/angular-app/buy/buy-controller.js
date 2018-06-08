bf079877486cedc0b0c4ef4890a63d27614486bb
Matthew Rizzini <matthewrizzini@gmail.com>
2018-06-08 23:40:54 +0000

    final change upper

--- public/angular-app/buy/buy-controller.js
+++ public/angular-app/buy/buy-controller.js
@@ -8,8 +8,9 @@ function BuyController($http, $window, AuthFactory, jwtHelper, $location) {
       var token = $window.sessionStorage.token;
       var decodedToken = jwtHelper.decodeToken(token);
       var username = decodedToken.username;
+      var symbolUpper = vm.symbol.toUpperCase();
       
-      var data = {"symbol" : vm.symbol.toUpper(), "amount": vm.amount
+      var data = {"symbol" : symbolUpper, "amount": vm.amount
       }
       
       $http.post('/api/users/'+ username +"/stocks", data).then(function(response) {
